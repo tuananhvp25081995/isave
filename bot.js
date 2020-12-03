fe0fb1bdccba73ccc3c5c1d5bf7aca32abcedb32
jasdasd <asdasd@asdasd.com>
2020-12-03 22:10:39 +0700

    increase send remind to 20msg/s

--- bot.js
+++ bot.js
@@ -518,7 +518,7 @@ sparkles.on("sendRemindHour", async () => {
                 "webminar.join_url": { $ne: "" },
                 "social.telegram.isBlock": false
             }, { telegramID: 1, webminar: 1, fullName: 1 })
-                .limit(15)
+                .limit(40)
             console.log(users);
             if (users.length) {
                 for (user of users) {
